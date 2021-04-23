f86e93fa857ec631b93f1b11753d2450230ad9e2
Hyun <itsmine096@gmail.com>
2021-04-23 18:37:42 +0900

    문제 6) Connection 정보관리 Object를 DataSource로 변경

--- java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -1,18 +1,42 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
 
+    @Value("${db.driver}")
+    private String className;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.url}")
+    private String url;
+
     @Bean
-    public UserDao userDao() {
-        return new UserDao(connectionMaker());
+    public UserDao userDao() throws ClassNotFoundException {
+        return new UserDao(dataSource());
     }
 
     @Bean
-    public JejuConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        return dataSource;
     }
 }
+//        return DriverManager.getConnection("jdbc:mysql://localhost:3306/portal_service?" +
+//                "characterEncoding=utf-8&serverTimezone=UTC", "jeju", "1234");
--- java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,16 +1,17 @@
 package kr.ac.jejunu.userdao;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -30,7 +31,7 @@ public class UserDao {
         return user;
     }
     public void insert(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo(name, password) VALUE (?,?)", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
         preparedStatement.setString(2, user.getPassword());
