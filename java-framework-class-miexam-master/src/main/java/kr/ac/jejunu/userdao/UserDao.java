ba2d747ad1c240e96b6122b39a93903039ab744a
Hyun <itsmine096@gmail.com>
2021-04-23 18:44:43 +0900

    문제 7) 종종 발생하는 장애 해결

--- java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -11,35 +11,83 @@ public class UserDao {
     }
 
     public User findById(Integer id) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement =
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try { connection = dataSource.getConnection();preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
 
-        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet = preparedStatement.executeQuery();
         resultSet.next();
 
-        User user = new User();
+        user = new User();
         user.setId(resultSet.getInt("id"));
         user.setName(resultSet.getString("name"));
         user.setPassword(resultSet.getString("password"));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            }
 
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-        //리턴
+        }
         return user;
     }
     public void insert(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo(name, password) VALUE (?,?)", Statement.RETURN_GENERATED_KEYS);
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+        connection = dataSource.getConnection();
+        preparedStatement = connection.prepareStatement("insert into userinfo(name, password) VALUE (?,?)", Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, user.getName());
         preparedStatement.setString(2, user.getPassword());
         preparedStatement.executeUpdate();
 
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet = preparedStatement.getGeneratedKeys();
         resultSet.next();
-
         user.setId(resultSet.getInt(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwable) {
+                throwable.printStackTrace();
+            }
+        }
+//        Connection connection = dataSource.getConnection();
+//        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo(name, password) VALUE (?,?)", Statement.RETURN_GENERATED_KEYS);
+//        preparedStatement.setString(1, user.getName());
+//        preparedStatement.setString(2, user.getPassword());
+//        preparedStatement.executeUpdate();
+//
+//        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+//        resultSet.next();
+//
+//        user.setId(resultSet.getInt(1));
+
+
     }
 }
