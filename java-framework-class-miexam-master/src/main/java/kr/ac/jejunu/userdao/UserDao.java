acb1b44b6b07837ecfc29274d26d4dce90ab903b
Hyun <itsmine096@gmail.com>
2021-04-23 16:58:59 +0900

    문제 1) 사용자 추가기능 추가

--- java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ java-framework-class-miexam-master/src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -3,11 +3,8 @@ package kr.ac.jejunu.userdao;
 import java.sql.*;
 
 public class UserDao {
-    public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/portal_service?" +
-                        "characterEncoding=utf-8&serverTimezone=UTC", "jeju","1234");
+    public User findById(Integer id) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -25,5 +22,21 @@ public class UserDao {
         connection.close();
         //리턴
         return user;
+    }
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo(name, password) VALUE (?,?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        user.setId(resultSet.getInt(1));
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/portal_service?" +
+                "characterEncoding=utf-8&serverTimezone=UTC", "jeju", "1234");
     }
 }
--- java-framework-class-miexam-master/src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
+++ java-framework-class-miexam-master/src/test/java/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -6,18 +6,38 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
     @Test
     public void testGet() throws SQLException, ClassNotFoundException {
         Integer id = 1;
-        String name = "hulk";
+        String name = "재현";
         String password = "1234";
         UserDao userDao = new UserDao();
-        User user = userDao.get(id);
+        User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
+    }
+
+    public void insert() throws SQLException, ClassNotFoundException {
+        String password = "1234";
+        String name = "재현";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(1);
+
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(insertedUser.getId(), is(user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
 }
